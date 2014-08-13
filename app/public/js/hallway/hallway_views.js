c6c8220f73e31ff959064dfe67b086d9e9391fd2
Nick Buchnan <nick@openhack.net>
2014-08-13 05:22:09 +0000

    keypress enter triggers room input event

--- app/public/js/hallway/hallway_views.js
+++ app/public/js/hallway/hallway_views.js
@@ -5,19 +5,25 @@ function HallwayViews(){
   var specialCharCode = {'8':'8', '13':'13', '32':'32', '186':'58', '187':'61', '188':'44', '189':'45', '190':'46', '191':'47', '192':'96', '219':'91', '220':'92', '221':'93', '222':'39'};
 
   var initialize = function(){
+    var $input = $('#roomnameinput');
     $('<div/>', {id:'local-container', class:'media-layout'})
       .append('<video id=\"local-video\" autoplay controls muted>')
       .append('<textarea id=\"local-ta\" placeholder="Being typing in real time"></textarea>')
       .appendTo('#video-container');
 
-    $('#roomnameinput').focus();
+    $input.focus();
+    $input.keypress(function(){
+      if (event.keyCode === 13){
+        $('#joinroombtn').trigger("click");
+      }
+    });
   };
 
   this.setListeners = function(engine){
     var sc = engine.sendChar;
     $('#local-ta').on('keydown', function textareaByteChar(e) {
       var code = (e.keyCode ? e.keyCode : e.which);
-      console.log(e.type, e.which, e.keyCode);
+      //console.log(e.type, e.which, e.keyCode);
 
       if( code == '37' || code == '38' || code == '39' || code == '40' ){
         e.preventDefault();
@@ -43,6 +49,7 @@ function HallwayViews(){
         };
       }
     })
+
   };
 
   this.openMediaViews = function(){
