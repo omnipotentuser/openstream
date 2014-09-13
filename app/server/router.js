f195453cc2034a5171c6e00b3693d068b8839f9d
Nick Buchnan <nick@openhack.net>
2014-09-13 04:40:11 +0000

    adding Lavatory link

--- app/server/router.js
+++ app/server/router.js
@@ -1,9 +1,12 @@
 module.exports = function(app){
   app.get('/', function(req, res){
-	  res.render('index', {title: 'OpenStream MCU server'});
+	  res.render('index', {title: 'OpenStream - Lobby'});
   });
 
   app.get('/hallway', function(req,res){
-	  res.render('index', {title: 'OpenStream MCU server'});
+	  res.render('index', {title: 'OpenStream - Hallway'});
+  });
+  app.get('/lavatory', function(req,res){
+	  res.render('index', {title: 'OpenStream - Lavatory'});
   });
 };
